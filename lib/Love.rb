a7298b76e311919e8e9589e74024aa16452bbf60
pingsukit <pingsukit@pingsukit.local>
2014-12-02 13:49:33 +0700

    edit2

--- lib/Love.rb
+++ lib/Love.rb
@@ -1,14 +1,15 @@
 class Love
-	def lovemac(number)
+	
+	def self.lovemac(number)
      show=""
      if number%3==0 && number%15!=0
      	show="Love"
      elsif number%5==0 && number%15!=0
      	show="Mac"
-     elsif number%15=0
+     elsif number%15==0
      	show="HateWindows"
      else 
-     	show=number+""
+     	show=number.to_s
      end
      show
 
--- spec/lib/Love_spec.rb
+++ spec/lib/Love_spec.rb
@@ -4,22 +4,23 @@ require "spec_helper"
 describe Love do 
 
 	it 'test case :multiples of three' do
-	    	
-		    expect(lovemac(3)).to eq(Love)
+
+	    	Love.lovemac(3).should eql "Love"
+		   #expect(lovemac(3)).to eq("Love")
 	end
 
 	it 'test case :multiples of five' do
 	    	
-		    expect(lovemac(5)).to eq(Mac)
+		    expect(Love.lovemac(5)).to eq("Mac")
 	end
 
 	it 'test case :multiples of five and three' do
 	    	
-		    expect(lovemac(15)).to eq(HateWindows)
+		    expect(Love.lovemac(15)).to eq("HateWindows")
 	end
 
 	it 'test case :not multiples of five or three' do
 	    	
-		    expect(lovemac(2)).to eq(HateWindows)
+		    expect(Love.lovemac(2)).to eq("2")
 	end
 end
